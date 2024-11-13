d69241b9c2027b279d69eb2066961c38d74f55d2
brianhabana <brianhabana@gmail.com>
2024-11-13 06:54:09 -0800

    Set Up Hero Camera

--- Source/GAS/Private/Characters/GASHeroCharacter.cpp
+++ Source/GAS/Private/Characters/GASHeroCharacter.cpp
@@ -1,8 +1,36 @@
 // Copyright River & Rain
 
-
 #include "Characters/GASHeroCharacter.h"
 #include "GASDebugHelper.h"
+
+AGASHeroCharacter::AGASHeroCharacter()
+{
+	// Initialize the capsule size for collision detection
+	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.f);
+
+	// Disable controller-based rotation for pitch, yaw, and roll
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	// Create and configure the camera boom (spring arm)
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(GetRootComponent()); // Attach camera boom to root component
+	CameraBoom->TargetArmLength = 200.f; // Set the camera boom distance from the character
+	CameraBoom->SocketOffset = FVector(0.f, 55.f, 65.f); // Set an offset for the camera position
+	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on controller input
+
+	// Create and configure the follow camera
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach camera to end of camera boom
+	FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate based on controller
+
+	// Configure character movement settings
+	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in direction of input
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 500.f, 0.f); // Configure the character's rotation speed
+	GetCharacterMovement()->MaxWalkSpeed = 400.f; // Configure the character's walking speed
+	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f; // Configure the deceleration when the character stops
+}
 
 void AGASHeroCharacter::BeginPlay()
 {
--- Source/GAS/Public/Characters/GASHeroCharacter.h
+++ Source/GAS/Public/Characters/GASHeroCharacter.h
@@ -4,16 +4,38 @@
 
 #include "CoreMinimal.h"
 #include "Characters/GASBaseCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 #include "GASHeroCharacter.generated.h"
 
 /**
  * 
  */
+class USpringArmComponent; 
+class UCameraComponent;
+
 UCLASS()
 class GAS_API AGASHeroCharacter : public AGASBaseCharacter
 {
 	GENERATED_BODY()
 
+public:
+	AGASHeroCharacter();
+	
 protected:
 	virtual void BeginPlay() override;
+
+private:
+#pragma region Components
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<USpringArmComponent> CameraBoom;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UCameraComponent> FollowCamera;
+	
+#pragma endregion
+
 };
