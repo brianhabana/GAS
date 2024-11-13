44594a4526d7f4f2158c1eadd977186aef49bad1
brianhabana <brianhabana@gmail.com>
2024-11-13 07:35:25 -0800

    Native Gameplay Tags

---
+++ Source/GAS/Private/GASGameplayTags.cpp
@@ -0,0 +1,14 @@
+// Copyright River & Rain
+
+
+#include "GASGameplayTags.h"
+
+
+namespace GASGameplayTags
+{
+	/** Input Tags **/
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Move, "InputTag.Move");
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Look, "InputTag.Look");
+
+
+}
---
+++ Source/GAS/Public/GASGameplayTags.h
@@ -0,0 +1,13 @@
+// Copyright River & Rain
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace GASGameplayTags
+{
+	/** Input Tags **/
+	GAS_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Move);
+	GAS_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Look);
+
+}
